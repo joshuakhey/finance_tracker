c11a1dc0d02a931dd4ff3f7e45330daecee7cc9b
joshuakhey <joshuahey05@gmail.com>
2026-09-17 23:24:22 -0400

    Fix duplicate SQLAlchemy instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,14 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import os
 
-db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',
-    'sqlite:////mnt/JHardDrive/data/jheypicloud/files/Finances/fintrack.db')
+        'sqlite:////mnt/JHardDrive/data/jheypicloud/files/Finances/fintrack.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    from .models import db
     db.init_app(app)
 
     with app.app_context():
