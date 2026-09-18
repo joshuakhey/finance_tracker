71b3dfd2a577ffeb3593389f0dc59cc924d384db
joshuakhey <joshuahey05@gmail.com>
2026-09-17 23:29:27 -0400

    Move db.create_all and seeding to run.py to prevent race condition

--- app/__init__.py
+++ app/__init__.py
@@ -22,9 +22,4 @@ def create_app():
         app.register_blueprint(investments_bp)
         app.register_blueprint(sync_bp)
 
-        db.create_all()
-        
-        from .services.categorizer import seed_default_categories
-        seed_default_categories()
-
     return app
--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 from app import create_app
+from app.models import db
+from app.services.categorizer import seed_default_categories
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()
+    seed_default_categories()
 
 if __name__ == '__main__':
     app.run(debug=True)
